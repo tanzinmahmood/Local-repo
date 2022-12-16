e12b4bbd3eb9cc61c86013a48d627a53c9018505
tanzinmahmood <mahmood.tanzin@gmail.com>
2022-12-15 16:50:18 -0800

    New File

---
+++ testing.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#### This is a new bash ####
+
+adduser taz1
+adduser taz2
+
+##### Creating a file\Folder #####
+
+mkdir -p /home/tanzin/bashscript
+
+touch /home/tanzin/bashscript/demo.txt
+
+##### Assigning Permission #####
+
+chmod 775 /home/tanzin/bashscript/demo.txt /home/tanzin/bashscript
+
+
